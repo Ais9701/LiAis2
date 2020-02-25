2971f3da5d9203a3b3f2934dd8fefcae621d53f7
Ais9701 <aysielu2016@gmail.com>
2020-02-25 15:39:31 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LiAis2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
